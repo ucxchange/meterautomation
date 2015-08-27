e3f22b94e77df8ccbb99f89e3277b27bdfd76545
Andy Anderson <andy@clubanderson.com>
2015-08-27 11:09:55 -0400

    more details

--- puppet/manifests/params.pp
+++ puppet/manifests/params.pp
@@ -8,14 +8,13 @@ class ucx-meter::params {
   $service = true
   $enable  = true
 
-  $sample_package_name = 'sample'
-  $sample_service_name = 'sample'
-  $sample_user_name    = 'root'
-  $sample_group_name   = 'root'
-  $sample_config_dir   = '/etc/sample'
-  $sample_config_file  = 'sample.conf'
-  $sample_sysconfig    = '/etc/sysconfig/sample'
-  $sample_logrotate    = '/etc/logrotate.d/sample'
-  $sample_options      = '-o sample'
+  $ucx_meter_package_name = 'ucx-meter'
+  $ucx_meter_service_name = 'ucx-meter'
+  $ucx_meter_location   = '/etc/ucx-meter'
+  $ucx_meter_config_file  = 'config.info'
+  $ucx_meter_options      = '-i infrastructure_sample_name'
+  $ucx_meter_user_name    = 'root'
+  $ucx_meter_group_name   = 'root'
+
 }
 # EOF
